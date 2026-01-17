9fc2e37246bf57bba369fe7c92a9fdea7a6645ac
ouneri <pasha0402@bk.ru>
2026-01-17 20:13:21 +0100

    db-json server

--- src/app/services/datasource.ts
+++ src/app/services/datasource.ts
@@ -1,106 +1,30 @@
 import { Injectable } from '@angular/core';
-import {of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Album} from '../interfaces/interface.album';
+import {Playlist} from '../interfaces/interface.playlist';
+import {Artist} from '../interfaces/interface.artist';
+import {Track} from '../interfaces/interface.track';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Datasource {
 
-  private artists = [{
-    id: 1,
-    name: 'elmora',
-    listeners: 143.767,
-    avatar: 'elmora-avatar.jpg'
+  private baseUrl = 'http://localhost:3000';
 
-  },
-    {
-      id: 2,
-      name: 'ouneri',
-      listeners: 10.235,
-      avatar: 'ouneri-avatar.jpg'
-
-    }
-  ];
-
-  private playlists = [
-    {
-      id: 1,
-      name: 'Mix day #1',
-      cover: '1.jpg',
-      description: 'Random tracks',
-      trackCount: 12
-    },
-    {
-      id: 2,
-      name: 'Mix day #2',
-      cover: '2.jpg',
-      description: 'Random tracks',
-      trackCount: 10
-    }
-  ];
-  private albums = [{
-    id: 1,
-    name: 'elmorapoppintramadol',
-    cover: '2.jpg',
-    artistIds: [1],
-    trackIds: [1, 2],
-
-  },
-    {
-      id: 2,
-      name: 'elmorapoppintramadol2',
-      cover: '11.jpg',
-      artistIds: [1],
-      trackIds: [1, 2],
-
-    },
-    {
-      id: 3,
-      name: 'elmorapoppintramadol3',
-      cover: '22.jpg',
-      artistIds: [1, 2],
-      trackIds: [1, 2],
-
-    },
-    {
-      id: 4,
-      name: 'elmorapoppintramadol3',
-      cover: '4.jpg',
-      artistIds: [1],
-      trackIds: [1, 2],
-
-    }
-
-  ]
-
-  private tracks = [{
-      id: 1,
-      name: 'Трамадольчик',
-      artistIds: [1],
-      img: '11.jpg',
-      durationSec: 120,
-
-    },
-    {
-      id: 2,
-      name: 'u',
-      artistIds: [1, 2],
-      img: '22.jpg',
-      durationSec: 120,
-    }
-  ]
+  constructor(private http: HttpClient) {}
 
   getAlbums() {
-    return of([...this.albums]);
+    return this.http.get<Album[]>(`${this.baseUrl}/albums`);
   }
   getPlaylists(){
-    return of([...this.playlists]);
+    return this.http.get<Playlist[]>(`${this.baseUrl}/playlists`);
   }
   getArtists(){
-    return of([...this.artists]);
+    return this.http.get<Artist[]>(`${this.baseUrl}/artists`);
   }
 
   getTracks() {
-    return of([...this.tracks]);
+    return this.http.get<Track[]>(`${this.baseUrl}/tracks`);
   }
 }
